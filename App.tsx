cf74bfe207a3232e228728f3e6b915199a0ef7f1
LeoZaiTech <98131015+LeoZaiTech@users.noreply.github.com>
2026-02-25 16:04:19 -0500

    Add auto-reset dailies on app launch, create presentation docs

--- App.tsx
+++ App.tsx
@@ -7,13 +7,18 @@ import { AppNavigator } from './src/navigation/AppNavigator';
 import { ErrorBoundary } from './src/components';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { StyleSheet } from 'react-native';
+import { resetDailies } from './src/store/dailies/dailiesSlice';
+
+const onBeforeLift = () => {
+  store.dispatch(resetDailies());
+};
 
 export default function App() {
   return (
     <GestureHandlerRootView style={styles.container}>
       <ErrorBoundary>
         <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
+          <PersistGate loading={null} persistor={persistor} onBeforeLift={onBeforeLift}>
             <StatusBar style="light" />
             <AppNavigator />
           </PersistGate>
